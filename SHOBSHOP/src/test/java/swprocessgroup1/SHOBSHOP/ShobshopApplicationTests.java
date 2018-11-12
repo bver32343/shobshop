78df19d8110334f956c0f3d1a95d3b04fa52c41a
bver32343 <42041335+bver32343@users.noreply.github.com>
2018-11-12 19:11:34 +0700

    Add files via upload

---
+++ SHOBSHOP/src/test/java/swprocessgroup1/SHOBSHOP/ShobshopApplicationTests.java
@@ -0,0 +1,16 @@
+package swprocessgroup1.SHOBSHOP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShobshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
