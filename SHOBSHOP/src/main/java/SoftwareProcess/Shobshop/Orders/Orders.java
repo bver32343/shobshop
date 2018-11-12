0c0f9a17e82c3a3e465361fba7e7b7610fcf20c7
DESKTOP-DJ040OI\suwat <suwat.pakhan@mail.kmutt.ac.th>
2018-11-12 21:27:27 +0700

    fix test

--- SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Orders/Orders.java
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Orders/Orders.java
@@ -1,10 +1,13 @@
 package SoftwareProcess.Shobshop.Orders;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
 
 @Entity 
 public class Orders {
+    @NotNull
     private int orderId ;
+    @NotNull
     private int quantity ;
     
 
