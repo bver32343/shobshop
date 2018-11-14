10ba5d7a61bc7aba5924d39a5018062d744c112a
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-14 11:59:38 +0700

    [create]AddressRepository

---
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package SoftwareProcess.Shobshop.Repository;
+
+import SoftwareProcess.Shobshop.Model.AddressModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<AddressModel, Integer>{
+    
+}
