cb8bc2e6fc5dfa4e7e098d0943856cbf5351ede6
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-14 11:59:56 +0700

    [create]AddressService

---
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Service/AddressService.java
@@ -0,0 +1,14 @@
+package SoftwareProcess.Shobshop.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import SoftwareProcess.Shobshop.Repository.AddressRepository;
+
+@Service
+public class AddressService{
+@Autowired
+AddressRepository addressRepository;
+
+
+}
