5224f3b20f4be56447d6823687185b9b64b70caa
DESKTOP-DJ040OI\suwat <suwat.pakhan@mail.kmutt.ac.th>
2018-11-14 13:49:00 +0700

    [fix]ordermodel

--- SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Model/OrderModel.java
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Model/OrderModel.java
@@ -20,7 +20,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
-@Table(name ="order")
+@Table(name ="orders")
 public class OrderModel {
 
 	@Id
