021cdc461c22ae29722aee528670989932cce703
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-15 17:41:49 +0700

    [add] method save in AddressService

--- SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Service/AddressService.java
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Service/AddressService.java
@@ -3,12 +3,15 @@ package SoftwareProcess.Shobshop.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import SoftwareProcess.Shobshop.Model.AddressModel;
 import SoftwareProcess.Shobshop.Repository.AddressRepository;
 
 @Service
 public class AddressService{
 @Autowired
-AddressRepository addressRepository;
+AddressRepository AddressRepository;
 
-
+    public AddressModel save(AddressModel address){
+        return AddressRepository.save(address);
+    }
 }
