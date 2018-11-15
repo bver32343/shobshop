717124d62ba31d3ae5ee06bd81770cafbb0aa668
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-15 17:42:15 +0700

    [add] method save in UserService

--- SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Service/UserService.java
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Service/UserService.java
@@ -2,12 +2,17 @@ package SoftwareProcess.Shobshop.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import SoftwareProcess.Shobshop.Model.UserModel;
 import SoftwareProcess.Shobshop.Repository.UserRepository;
 
 @Service
 public class UserService{
     @Autowired
     UserRepository UserRepository;
+    public UserModel save(UserModel users){
+        return UserRepository.save(users);
 
+    }
 
 }
