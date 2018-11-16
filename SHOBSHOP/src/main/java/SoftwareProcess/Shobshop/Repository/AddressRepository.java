c8a9d96520cea3fbfd2fe91d16a0f698423306a3
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-16 17:13:17 +0700

    [add] method variable addressmodel

--- SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Repository/AddressRepository.java
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Repository/AddressRepository.java
@@ -4,5 +4,5 @@ import SoftwareProcess.Shobshop.Model.AddressModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<AddressModel, Integer>{
-    
+    public AddressModel findByAddressId(int addressId);
 }
