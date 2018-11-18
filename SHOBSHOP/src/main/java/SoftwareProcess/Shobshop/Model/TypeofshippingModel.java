311f541121eae876858c096d9a0a9dcd09b067d9
sarut093 <sarut.rek@mail.kmutt.ac.th>
2018-11-19 01:42:02 +0700

    [create] TypeofshippingModel

---
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Model/TypeofshippingModel.java
@@ -0,0 +1,41 @@
+package SoftwareProcess.Shobshop.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "typeofshipping")
+
+public class TypeofshippingModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int typeOfShippingId;
+    
+    @NotBlank
+    private String typeOfShippingName;
+
+	public int getTypeOfShippingId()
+	{
+		return this.typeOfShippingId;
+	}
+
+	public void setTypeOfShippingId(int typeOfShippingId)
+	{
+		this.typeOfShippingId = typeOfShippingId;
+	}
+
+	public String getTypeOfNhippingName()
+	{
+		return this.typeOfShippingName;
+	}
+
+	public void setTypeOfShippingName(String typeOfShippingName)
+	{
+		this.typeOfShippingName = typeOfShippingName;
+	}
+
+}
