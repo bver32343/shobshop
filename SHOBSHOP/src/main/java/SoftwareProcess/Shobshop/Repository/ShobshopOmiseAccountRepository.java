027dd236b5effce4db33b1bdca1572ef7265f5b3
Unknown <saknarong.ball@mail.kmutt.ac.th>
2018-11-19 22:46:27 +0700

    [add] Omise repo

---
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Repository/ShobshopOmiseAccountRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoftwareProcess.Shobshop.Repository;
+
+import SoftwareProcess.Shobshop.Model.ProductModel;
+import SoftwareProcess.Shobshop.Model.ShobshopOmiseAccountModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author saknarong
+ */
+public interface ShobshopOmiseAccountRepository extends JpaRepository<ShobshopOmiseAccountModel, Integer> {
+
+    ShobshopOmiseAccountModel findByShobshopOmiseAccountId(int shobshopOmiseAccountId);
+}
