f713db722b6de8e5b28d0ffa60cc5e0957bd5234
Unknown <saknarong.ball@mail.kmutt.ac.th>
2018-11-19 22:45:28 +0700

    [Add] class to cennectOmise

---
+++ SHOBSHOP/src/main/java/SoftwareProcess/Shobshop/Controller/ShobshopOmiseAccountController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoftwareProcess.Shobshop.Controller;
+
+import SoftwareProcess.Shobshop.Model.ProductModel;
+import SoftwareProcess.Shobshop.Model.ShobshopOmiseAccountModel;
+import SoftwareProcess.Shobshop.Service.ProductService;
+import SoftwareProcess.Shobshop.Service.ShobshopOmiseAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author saknarong
+ */
+@Controller
+public class ShobshopOmiseAccountController {
+
+
+}
